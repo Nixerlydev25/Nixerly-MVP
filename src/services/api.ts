f78348aafe68e5b4f0cfcadfa345e49016d8f3c8
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-05-30 21:58:57 +0500

    ci trigger

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 const instance = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL,
   baseURL: "https://api.nixerly.com/v1",
+  
   withCredentials: true,
 });
 

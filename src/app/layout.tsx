9c2e0c678e5cb743b65567a7c95d1cfe5cf0b7fd
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-08-20 00:50:20 +0500

    dissabled zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
       </head>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} ${inter.className} ${poppins.className}`}
